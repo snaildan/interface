9925b53cdbcb29b22e2332d131ca4926cd3a0271
zhangdan <zhangdan@gmw.cn>
2019-04-19 16:29:27 +0800

    初始版本上传

---
+++ src/main/java/cn/gmw/api/meiyou/MeiyouApplication.java
@@ -0,0 +1,14 @@
+package cn.gmw.api.meiyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeiyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeiyouApplication.class, args);
+        //System.out.print("hello");
+    }
+
+}
---
+++ src/test/java/cn/gmw/api/meiyou/MeiyouApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.gmw.api.meiyou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeiyouApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
